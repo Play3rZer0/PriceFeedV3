eae6a80810205f65723d07b1c265cb3de07df95d
VinceT <37818562+Play3rZer0@users.noreply.github.com>
2024-08-28 12:59:43 +0800

    Update PirceFeedV3.sol

--- PirceFeedV3.sol
+++ PirceFeedV3.sol
@@ -1,1 +1,50 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import {AggregatorV3Interface} from "@chainlink/contracts@1.2.0/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract PriceFeedV3 {
+    AggregatorV3Interface internal dataFeed;
+    AggregatorV3Interface internal dataFeed2;
+
+    /**
+     * Network: Sepolia
+     * Aggregator: BTC/USD and ETH/USD
+     * 
+     */
+    constructor() {
+        dataFeed = AggregatorV3Interface(
+            0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43
+        );
+        dataFeed2 = AggregatorV3Interface(
+            0x694AA1769357215DE4FAC081bf1f309aDC325306
+        );
+    }
+
+    /**
+     * Returns the latest answer.
+     */
+    function getLatestBitcoinPrice() public view returns (int) {
+        // prettier-ignore
+        (
+            /* uint80 roundID */,
+            int answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return answer;
+    }
+
+    function getLatestETHPrice() public view returns (int) {
+        // prettier-ignore
+        (
+            /* uint80 roundID */,
+            int answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = dataFeed2.latestRoundData();
+        return answer;
+    }
+}
